e6a07c58d34db8cc5dd61a9f2314498f8b82407c
Cristian <389336@alu.murciaeduca.es>
2023-11-13 13:34:53 +0100

    Cambiamos el compileSdka version 34 en Gradel

---
+++ app/src/main/java/com/proyecto/futbolmanagerpro/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.proyecto.futbolmanagerpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.proyecto.futbolmanagerpro.ui.theme.FutbolManagerProTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FutbolManagerProTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Master()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Master() {
+    Text(text = "Master")
+    Text(text = "vacio")
+}
